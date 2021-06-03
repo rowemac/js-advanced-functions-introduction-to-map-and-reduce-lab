cbba27bafd64bf58aeaf4fcf72a003b63f8b7c26
Mac Rowe <m.rowe42@gmail.com>
2021-06-03 10:19:54 -0400

    commit tree 88ba3b8e6b440fbb39adc4432b6f1d57b5a85427

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(array) {
+    return array.map(x => x * -1)
+}
+
+function mapToNoChange(array) {
+    return array.map(x => x)
+}
+
+function mapToDouble(array) {
+    return array.map(x => x * 2)
+}
+
+function mapToSquare(array) {
+    return array.map(x => x ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let total = startingPoint
+    for(let num of sourceArray){
+        total = total + num 
+    }
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let value = true
+    for(let element of sourceArray){
+        if(element === false) {
+            value = false
+        }
+    }
+    return value 
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let value = false
+    for(let element of sourceArray) {
+        if(element === true) {
+            value = true
+        }
+    }
+    return value
+}
